971f0debc2aaa9b0bc82e021dafabe6b898c7e64
Vincenzo Favara <vivi@iMpbVinc.local>
2014-11-01 18:01:37 +0100

    fix list clues

--- GoogleTreasureHunt/ListViewController.swift
+++ GoogleTreasureHunt/ListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Foundation
 
-class ListViewController : UIViewController, UITableViewDelegate {
+class ListViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
    
     @IBOutlet weak var tableView: UITableView!
     
@@ -18,17 +18,21 @@ class ListViewController : UIViewController, UITableViewDelegate {
     var hunt:Hunt = Hunt()
     
     var clues: Array<Clue> = Array<Clue>()
+    
     override func viewDidLoad() {
-        //super.viewDidLoad()
         self.tableView.delegate = self
-        //self.tableView.dataSource = self
+        self.tableView.dataSource = self
     }
     
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if clues.isEmpty {
+            hunt.createHunt()
+            clues = hunt.clueListShuffleFinal
+        }
+        return clues.count
     }
     
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         if clues.isEmpty {
             hunt.createHunt()
@@ -46,9 +50,35 @@ class ListViewController : UIViewController, UITableViewDelegate {
         cell.backgroundColor = color
         return cell
     }
-    
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+/*
+    func tableView(tableView: UITableView!, titleForHeaderInSection section: Int!) -> String! {
         return tableViewtitle
     }
+
+    func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 0
+    }
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Test\(indexPath.section) \(indexPath.row)")
+    }
+    
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {}
+    
+    func pickerView(pickerView: UIPickerView!,numberOfRowsInComponent component: Int) -> Int{}
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?{
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?{
+    }
+*/
     
 }
+
